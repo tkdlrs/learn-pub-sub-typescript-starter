b028e0a376d9dd1ca9da6f0bce7b372c936a1afb
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-20 19:57:59 -0600

    Completes 'Chapter 2: Message Brokers | Lesson 4: Connect' of the 'Learn Pub/Sub in RabbitMQ and TypeScript' course on Boot.dev.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,20 @@
+import amqp from 'amqplib';
+import process from 'node:process';
+//
 async function main() {
-  console.log("Starting Peril server...");
+    console.log('Starting Peril server...');
+    const rabbitConnString = 'amqp://guest:guest@localhost:5672/';
+    const conn = await amqp.connect(rabbitConnString);
+    //
+    console.log(`Connection was successful`);
+    //
+    process.on('SIGINT', () => {
+        console.log(`Received SIGINT. Program is shutting down.`);
+        conn.close();
+    });
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
+    console.error('Fatal error:', err);
+    process.exit(1);
 });
