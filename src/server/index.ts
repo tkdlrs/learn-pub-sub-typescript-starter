8f64ca287f4dae87804a128869bafa03b00c7401
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-20 20:07:25 -0600

    Reviews the solution files for 'Chapter 2: Message Brokers | Lesson 4: Connect' of the 'Learn Pub/Sub in RabbitMQ and TypeScript' course on Boot.dev.

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,14 +3,24 @@ import process from 'node:process';
 //
 async function main() {
     console.log('Starting Peril server...');
+    //
     const rabbitConnString = 'amqp://guest:guest@localhost:5672/';
     const conn = await amqp.connect(rabbitConnString);
+    console.log(`Peril game server connect to RabbitMQ was successful`);
     //
-    console.log(`Connection was successful`);
-    //
-    process.on('SIGINT', () => {
-        console.log(`Received SIGINT. Program is shutting down.`);
-        conn.close();
+    ['SIGINT', 'SIGTERM'].forEach((signal) => {
+        process.on(signal, async () => {
+            try {
+                await conn.close();
+                console.log(
+                    `RabbitMQ connection closed. Program shutting down.`,
+                );
+            } catch (err) {
+                console.error(`Error closing RabbitMQ connection: ${err}`);
+            } finally {
+                process.exit(0);
+            }
+        });
     });
 }
 
