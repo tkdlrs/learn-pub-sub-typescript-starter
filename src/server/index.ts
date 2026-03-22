462971b7f4974b06d700c65413ffb4be24e42129
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-22 17:48:11 -0600

    Completes 'Chapter 3: Publishers and Queues | Lesson 1: Exchanges and Queues' of the 'Learn Pub/Sub in RabbitMQ and TypeScript' course on Boot.dev.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,21 @@
 import amqp from 'amqplib';
 import process from 'node:process';
+import { publishJSON } from '../internal/pubsub/publishjson.js';
+import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js';
 //
 async function main() {
     console.log('Starting Peril server...');
     //
     const rabbitConnString = 'amqp://guest:guest@localhost:5672/';
     const conn = await amqp.connect(rabbitConnString);
+    const confirmCannel = await conn.createConfirmChannel();
     console.log(`Peril game server connect to RabbitMQ was successful`);
+    //
+    publishJSON(confirmCannel, ExchangePerilDirect, PauseKey, {
+        isPaused: true,
+    });
+    //
+    //
     //
     ['SIGINT', 'SIGTERM'].forEach((signal) => {
         process.on(signal, async () => {
