b1c33961c3527ddbc105f0c18d32e6cbc174a60c
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-24 19:07:50 -0600

    Completes 'Chapter 3: Publishers and Queues | Lesson 5: Transient Queues' of the 'Learn Pub/Sub in RabbitMQ and TypeScript' course on Boot.dev

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,29 @@
+import amqp from 'amqplib';
+import { clientWelcome } from '../internal/gamelogic/gamelogic.js';
+import {
+    declareAndBind,
+    SimpleQueueType,
+} from '../internal/pubsub/decareAndBind.js';
+import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js';
+//
 async function main() {
-  console.log("Starting Peril client...");
+    console.log('Starting Peril client...');
+    //
+    const rabbitConnString = 'amqp://guest:guest@localhost:5672/';
+    const conn = await amqp.connect(rabbitConnString);
+    //
+    const username = await clientWelcome();
+    //
+    declareAndBind(
+        conn,
+        ExchangePerilDirect,
+        `pause.${username}`,
+        PauseKey,
+        SimpleQueueType.Transient,
+    );
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
+    console.error('Fatal error:', err);
+    process.exit(1);
 });
