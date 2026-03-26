f11f0bcd2a504bb363d5e1657fa7e467ec78afc2
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-25 19:57:44 -0600

    Completes 'Chapter 3: Publishers and Queues | Lesson 6: Decoupling' of the 'Learn Pub/Sub in RabbitMQ and TypeScript' course on Boot.dev.

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,28 @@ import amqp from 'amqplib';
 import process from 'node:process';
 import { publishJSON } from '../internal/pubsub/publish.js';
 import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js';
+import { getInput, printServerHelp } from '../internal/gamelogic/gamelogic.js';
+//
+async function pauseAction(channel: amqp.ConfirmChannel): Promise<void> {
+    try {
+        await publishJSON(channel, ExchangePerilDirect, PauseKey, {
+            isPaused: true,
+        });
+    } catch (err) {
+        console.error(`Error publishing message: ${err}`);
+    }
+    return;
+} //
+async function resumeAction(channel: amqp.ConfirmChannel): Promise<void> {
+    try {
+        await publishJSON(channel, ExchangePerilDirect, PauseKey, {
+            isPaused: false,
+        });
+    } catch (err) {
+        console.error(`Error publishing message: ${err}`);
+    }
+    return;
+}
 //
 async function main() {
     const rabbitConnString = 'amqp://guest:guest@localhost:5672/';
@@ -24,13 +46,36 @@ async function main() {
     });
     //
     const publishCh = await conn.createConfirmChannel();
-    try {
-        await publishJSON(publishCh, ExchangePerilDirect, PauseKey, {
-            isPaused: true,
-        });
-    } catch (err) {
-        console.error(`Error publishing message: ${err}`);
+    //
+    printServerHelp();
+    aLoop: while (true) {
+        const arrWords = await getInput();
+        if (arrWords.length === 0) {
+            continue;
+        }
+        //
+        const firstWord = arrWords[0];
+        console.log(`firstWord: ${firstWord}`);
+        //
+        switch (firstWord) {
+            case 'pause':
+                await pauseAction(publishCh);
+                break;
+            case 'resume':
+                await resumeAction(publishCh);
+                break;
+            case 'quit':
+                console.log('I am out of here');
+                break aLoop;
+            default:
+                console.log(`I don't understand...`);
+                break;
+        }
+        //
     }
+    return;
+    //
+
     //
 }
 
