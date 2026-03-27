0c4f35c07d0b466f73e88c133ae71bd35831f44f
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-26 20:37:34 -0600

    Completes 'Chapter 3: Publishers and Queues | Lesson 7: Client REPL' of the 'Learn Pub/Sub in RabbitMQ and TypeScript' course on Boot.dev.

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,18 @@
 import amqp from 'amqplib';
-import { clientWelcome } from '../internal/gamelogic/gamelogic.js';
+import {
+    clientWelcome,
+    commandStatus,
+    getInput,
+    printClientHelp,
+    printQuit,
+} from '../internal/gamelogic/gamelogic.js';
 import { declareAndBind, SimpleQueueType } from '../internal/pubsub/consume.js';
 import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js';
+import { GameState } from '../internal/gamelogic/gamestate.js';
+import { commandSpawn } from '../internal/gamelogic/spawn.js';
+import { commandMove } from '../internal/gamelogic/move.js';
+//
+
 //
 async function main() {
     const rabbitConnString = 'amqp://guest:guest@localhost:5672/';
@@ -30,6 +41,49 @@ async function main() {
         PauseKey,
         SimpleQueueType.Transient,
     );
+    //
+    const gameState = new GameState(username);
+    //
+    outerloop: while (true) {
+        const words = await getInput();
+        if (words.length === 0) {
+            continue;
+        }
+        //
+        const command = words[0];
+        //
+        switch (command) {
+            case 'spawn':
+                console.log(`Calling "commandSpawn"`);
+                commandSpawn(gameState, words);
+                break;
+            case 'move':
+                console.log(`Calling "commandMove"`);
+                commandMove(gameState, words);
+                console.log(`move appears to be a success`);
+                break;
+            case 'status':
+                console.log(`Calling "commandStatus"`);
+                commandStatus(gameState);
+                break;
+            case 'help':
+                console.log(`Calling "printClientHelp"`);
+                printClientHelp();
+                break;
+            case 'spam':
+                console.log(`Spamming not allowed yet!`);
+                break;
+            case 'quit':
+                console.log(`Calling "printQuit"`);
+                printQuit();
+                process.exit(0);
+                break;
+            default:
+                console.log(`error: WTF "${command}" is not a command.`);
+                continue outerloop;
+                break;
+        }
+    }
 }
 
 main().catch((err) => {
